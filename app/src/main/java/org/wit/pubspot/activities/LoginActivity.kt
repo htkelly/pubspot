fced00b15c15665620a85cd7bafdeb13c97644a6
htkelly <hrykly@gmail.com>
2022-10-30 15:47:03 +0000

    Introduced basic login/signup UI -- pubspot data is not user specific yet

---
+++ app/src/main/java/org/wit/pubspot/activities/LoginActivity.kt
@@ -0,0 +1,67 @@
+package org.wit.pubspot.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
+import com.squareup.picasso.Picasso
+import org.wit.pubspot.R
+import org.wit.pubspot.databinding.ActivityLoginBinding
+import org.wit.pubspot.helpers.authenticate
+import org.wit.pubspot.main.MainApp
+import org.wit.pubspot.models.UserModel
+import timber.log.Timber
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+    private lateinit var pubListIntentLauncher : ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding.loginToolbar.title = title
+        setContentView(binding.root)
+
+        val app = application as MainApp
+
+        registerPubListIntentLauncher()
+
+        binding.signupBtn.setOnClickListener(){
+            val signupUser = UserModel(email = binding.loginEmail.text.toString(), password = binding.loginPassword.text.toString())
+            val launcherIntent = Intent(this, PubListActivity::class.java)
+            app.users.create(signupUser)
+            app.users.logAll()
+            launcherIntent.putExtra("loggedInUser", signupUser)
+            pubListIntentLauncher.launch(launcherIntent)
+        }
+
+        binding.loginBtn.setOnClickListener(){
+            val loginUser = authenticate(binding.loginEmail.text.toString(), binding.loginPassword.text.toString(), app.users)
+            val launcherIntent = Intent(this, PubListActivity::class.java)
+            if (loginUser == null) {
+                Snackbar.make(it, R.string.login_error, Snackbar.LENGTH_LONG)
+                    .show()
+            }
+            else {
+                launcherIntent.putExtra("loggedInUser", loginUser)
+                pubListIntentLauncher.launch(launcherIntent)
+            }
+        }
+    }
+
+    private fun registerPubListIntentLauncher() {
+        pubListIntentLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+        //This doesn't do anything right now, but might be useful later
+        { result ->
+            when(result.resultCode){
+                RESULT_OK -> {}
+                RESULT_CANCELED -> { }
+                else -> { }
+            }
+        }
+    }
+
+}
--- app/src/main/java/org/wit/pubspot/main/MainApp.kt
+++ app/src/main/java/org/wit/pubspot/main/MainApp.kt
@@ -4,12 +4,14 @@ import android.app.Application
 import org.wit.pubspot.models.PubspotJsonStore
 import org.wit.pubspot.models.PubspotMemStore
 import org.wit.pubspot.models.PubspotStore
+import org.wit.pubspot.models.UserMemStore
 import timber.log.Timber
 import timber.log.Timber.i
 
 class MainApp: Application() {
 
     lateinit var pubs: PubspotStore
+    val users = UserMemStore()
 
     override fun onCreate() {
         super.onCreate()
