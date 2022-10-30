d5e15735431062e6bd885d4e1ea90f620a0a27e0
htkelly <hrykly@gmail.com>
2022-10-30 15:50:44 +0000

    Simplified user model

--- app/src/main/java/org/wit/pubspot/models/UserModel.kt
+++ app/src/main/java/org/wit/pubspot/models/UserModel.kt
@@ -5,8 +5,7 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class UserModel(var id: Long = 0,
-                        var firstName: String = "",
-                        var lastName: String = "",
                         var email: String = "",
-                        var password: String = "") : Parcelable
+                        var password: String = "",
+                        var pubs : ArrayList<PubspotModel> = ArrayList()) : Parcelable
 
